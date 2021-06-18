18b605416d1165b383755342613a12a80d1e9d38
Katarine Albuquerque <kba.2879@gmail.com>
2021-06-18 13:08:12 -0300

    commit

---
+++ src/components/blocos/Home/Videos/VideosData.js
@@ -0,0 +1,64 @@
+import Download02Img from '../../../../imagens/Home/HomeEducacional/download-4.svg';
+import CrockImg from '../../../../imagens/Home/HomeEducacional/clock.svg';
+import BookmarkImg from '../../../../imagens/Home/HomeEducacional/bookmark.svg';
+import Imagem01 from '../../../../imagens/Home/HomeEducacional/offset_comp_218310-1.png';
+import Imagem02 from '../../../../imagens/Home/HomeEducacional/offset_comp_824604.png';
+import Imagem03 from '../../../../imagens/Home/HomeEducacional/offset_comp_680339.png';
+import Imagem04 from '../../../../imagens/Home/HomeEducacional/offset_comp_653229-1.png';
+
+export const VideosData = [
+    {
+        imagem: Imagem01,
+        icon1: BookmarkImg,
+        icon2: Download02Img,
+        icon3: CrockImg,
+        textoIcon3: '3',   
+        id: '3',
+        titulo: "Vieses comportamentais",
+    },
+    {
+        imagem: Imagem02,
+        icon1: BookmarkImg,
+        icon2: Download02Img,
+        icon3: CrockImg,
+        textoIcon3: '3',   
+        id: '4',
+        titulo: "Vieses comportamentais",
+    },
+    {
+        imagem: Imagem03,
+        icon1: BookmarkImg,
+        icon2: Download02Img,
+        icon3: CrockImg,
+        textoIcon3: '3',   
+        id: '5',
+        titulo: "Vieses comportamentais",
+    },
+    {
+        imagem: Imagem04,
+        icon1: BookmarkImg,
+        icon2: Download02Img,
+        icon3: CrockImg,
+        textoIcon3: '3',   
+        id: '6',
+        titulo: "Vieses comportamentais",
+    },
+    {
+        imagem: Imagem02,
+        icon1: BookmarkImg,
+        icon2: Download02Img,
+        icon3: CrockImg,
+        textoIcon3: '3',   
+        id: '7',
+        titulo: "Vieses comportamentais",
+    },
+    {
+        imagem: Imagem01,
+        icon1: BookmarkImg,
+        icon2: Download02Img,
+        icon3: CrockImg,
+        textoIcon3: '3',   
+        id: '8',
+        titulo: "Vieses comportamentais",
+    },
+];
