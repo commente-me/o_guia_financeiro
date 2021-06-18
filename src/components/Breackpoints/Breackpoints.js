de358cc93ec040ef624e5ab81d93fb0b98836be4
Katarine Albuquerque <kba.2879@gmail.com>
2021-06-18 15:01:42 -0300

    commit

--- src/components/Breackpoints/Breackpoints.js
+++
@@ -1,7 +0,0 @@
-export const BreakPointsData = [
-    { width: 580, itemsToShow: 1 },
-    { width: 620, itemsToShow: 2 },
-    { width: 768, itemsToShow: 2 },
-    { width: 991, itemsToShow: 3 },
-    { width: 1100, itemsToShow: 4 },
-]
