7c7062cd4d279cc093e84a293af2bd00e9864347
Jose Luiz Rodrigues <zeluizr@me.com>
2021-06-22 13:25:01 -0400

    commit first slide

--- resources/scripts/guide.js
+++ resources/scripts/guide.js
@@ -4,39 +4,32 @@
   //   speed: 1,
   //   pauseOnHover: true,
   // });
-})();
 
-$(document).ready(function () {
-  // $(".responsive").slick({
-  //   dots: true,
-  //   infinite: false,
-  //   speed: 300,
-  //   slidesToShow: 4,
-  //   slidesToScroll: 4,
-  //   responsive: [
-  //     {
-  //       breakpoint: 1024,
-  //       settings: {
-  //         slidesToShow: 3,
-  //         slidesToScroll: 3,
-  //         infinite: true,
-  //         dots: true,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 600,
-  //       settings: {
-  //         slidesToShow: 2,
-  //         slidesToScroll: 2,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1,
-  //       },
-  //     },
-  //   ],
-  // });
-});
+  const swiper = new Swiper(".swiper-container", {
+    slidesPerView: 2,
+    spaceBetween: 15,
+    grabCursor: true,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+      1220: {
+        slidesPerView: 4,
+      },
+    },
+  });
+})();
