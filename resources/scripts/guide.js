31c9c54c710f7fbf1fb98bff07e3a7e17d1aa3a9
Jose Luiz Rodrigues <zeluizr@me.com>
2021-06-25 19:58:20 -0400

    container page complete with grid css

--- resources/scripts/guide.js
+++ resources/scripts/guide.js
@@ -4,37 +4,4 @@
   //   speed: 1,
   //   pauseOnHover: true,
   // });
-
-  const swiper_you_watching = new Swiper(".gd-you-watching-episode-container", {
-    slidesPerView: "auto",
-    spaceBetween: 15,
-    grabCursor: true,
-    freeMode: true,
-    pagination: {
-      el: ".gd-you-watching-episode-swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".gd-you-watching-episode-button-next",
-      prevEl: ".gd-you-watching-episode-button-prev",
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 15,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 15,
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 15,
-      },
-      1220: {
-        slidesPerView: 4,
-        spaceBetween: 15,
-      },
-    },
-  });
 })();
