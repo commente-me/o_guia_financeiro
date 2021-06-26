997de0c44fda2a077ca049ebfdcee0afeb79e142
Jose Luiz Rodrigues <zeluizr@me.com>
2021-06-26 16:17:11 -0400

    menu home slide

--- resources/scripts/guide.js
+++ resources/scripts/guide.js
@@ -1,7 +1,20 @@
+const menu = {
+	floatingMenu: document.querySelector(".gd-floating-menu"),
+	blackOut: document.querySelector(".gd-blackout"),
+	open: () => {
+		menu.floatingMenu.style.width = "295px";
+		menu.blackOut.style.display = "flex";
+	},
+	close: () => {
+		menu.floatingMenu.style.width = 0;
+		menu.blackOut.style.display = "none";
+	},
+};
+
 (async () => {
-  // nodeMarquee({
-  //   selector: "#marquee",
-  //   speed: 1,
-  //   pauseOnHover: true,
-  // });
+	// nodeMarquee({
+	//   selector: "#marquee",
+	//   speed: 1,
+	//   pauseOnHover: true,
+	// });
 })();
